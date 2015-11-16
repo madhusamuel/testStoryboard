c5310435bdba1c3e82064e80816a88991ffb6f14
Madhu Samuel <madhusamuel@gmail.com>
2015-11-17 10:38:09 +1100

    Initial commit

---
+++ teststoryboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  teststoryboard
+//
+//  Created by Madhu Samuel on 17/11/2015.
+//  Copyright © 2015 Madhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
