1925077fd42efb8a78c0f20fe01a8f37248925a0
Madhu Samuel <madhusamuel@gmail.com>
2015-11-17 12:37:54 +1100

    Added new swift class - SecondViewController.

---
+++ teststoryboard/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  teststoryboard
+//
+//  Created by Madhu Samuel on 17/11/2015.
+//  Copyright © 2015 Madhu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+}
